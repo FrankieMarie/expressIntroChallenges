3df49381a1caa799b262359009dadf11b8d3a15f
FrankieMarie <frankiemarie83@gmail.com>
2018-03-29 13:16:39 -0700

    part one

--- part1/server.js
+++ part1/server.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var app = express();
-var port = process.env.PORT || 8000;
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 8000;
 
 app.get('/yourroute', function(req, res) {
   res.send("stuff");
+});
+
+app.get('/hello', function(req, res) {
+  res.send("hello");
+});
+
+app.post('/create/:name', function(req, res) {
+  let obj = `${req.params.name}`;
+  res.json(obj);
+})
+
+app.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.get('/verify/:age', function(req, res) {
+  if(`${req.params.age}` > 13){
+    res.send("all good");
+  }else{
+    res.status(403).render();
+  }
+
 });
 
 app.use(function(req, res) {
